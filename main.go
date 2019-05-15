9ac460037e0dab45c9ee06c84daf6eca398cb465
常文才 <771826560@qq.com>
2019-05-15 09:24:12 +0800

    fix useless

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	beego.SetLogger("file", `{"filename":"logs/gumei.log"}`)
 	logs.EnableFuncCallDepth(true)
 
-	beego.SetStaticPath("static", "static")
+	//beego.SetStaticPath("static", "static")
 	beego.Run()
 }
 
